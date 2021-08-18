c7dad87c7e2eece0c71916c4a136c9592395a4f8
Gazelle Lowcay <61642400+elle-zed12@users.noreply.github.com>
2021-08-18 23:52:05 +1200

    First commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route, Switch
-}from 'react-router-dom';
+} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Header from './pages/Header';
 import Footer from './pages/Footer';
 import Facilities from './pages/Facilities';
-import HouseRules from './pages/House-Rules';
-import ContactUs from './pages/Contact-Us';
-import AboutUs from './pages/About-Us';
-import FeesPayments from './pages/Fees-payments';
+import HouseRules from './pages/HouseRules';
+import ContactUs from './pages/ContactUs';
+import AboutUs from './pages/AboutUs';
+import FeesPayments from './pages/FeesPayments';
 import NotFoundpage from './pages/NotFoundPage';
-import ApplyNow from './pages/Apply-Now';
+import ApplyNow from './pages/ApplyNow';
 
 // import Footer from './pages/Footer';
 
@@ -23,23 +23,23 @@ function App() {
       <div className="App" >
         <Header />
         <div id="page-body">
-      <Switch>
-       <Route path="/" component={Home}  exact />
-          <Route path="/home" component={Home}   />
-          <Route path="/facilities" component={Facilities}  />
-          <Route path="/house-rules" component={HouseRules}  />
-          <Route path="/about-us" component={AboutUs}  />
-          <Route path="/fees-payments" component={FeesPayments}  /> 
-          <Route path="/contact-us" component={ContactUs}  /> 
-          <Route path="/apply-now" component={ApplyNow}  /> 
-          <Route component={NotFoundpage}  />
-       </Switch> 
-          
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/home" component={Home} />
+            <Route path="/facilities" component={Facilities} />
+            <Route path="/house-rules" component={HouseRules} />
+            <Route path="/about-us" component={AboutUs} />
+            <Route path="/fees-payments" component={FeesPayments} />
+            <Route path="/contact-us" component={ContactUs} />
+            <Route path="/apply-now" component={ApplyNow} />
+            <Route component={NotFoundpage} />
+          </Switch>
+
         </div>
         <Footer />
       </div>
     </Router>
-  
+
   );
 }
 
--- src/pages/Facilities.js
+++ src/pages/Facilities.js
@@ -7,103 +7,99 @@ import ImageGallery from 'react-image-gallery';
 
 
 const images = [
-    {
-      original: "https://i.postimg.cc/7LvNN50s/kitchen.jpg",
-      thumbnail: "https://i.postimg.cc/7LvNN50s/kitchen.jpg",
-    },
-    {
-      original: 'https://i.postimg.cc/pT5hcCvK/living-Room.jpg',
-      thumbnail: 'https://i.postimg.cc/pT5hcCvK/living-Room.jpg',
-    },
-    {
-      original: 'https://i.postimg.cc/PrVLH0q1/lounge2.jpg"',
-      thumbnail: 'https://i.postimg.cc/PrVLH0q1/lounge2.jpg"',
-    },
-    {
-        original: "https://i.postimg.cc/L8FqCLQ1/friends.jpg",
-        thumbnail: "https://i.postimg.cc/L8FqCLQ1/friends.jpg",
-      },
-      {
-        original: 'https://i.postimg.cc/vZnrDGkW/room.jpg',
-        thumbnail: 'https://i.postimg.cc/vZnrDGkW/room.jpg',
-      },
-      {
-        original: 'https://i.postimg.cc/yYD1mxVS/happy-Insidethe-Room.jpg',
-        thumbnail: 'https://i.postimg.cc/yYD1mxVS/happy-Insidethe-Room.jpg',
-      },
-      {
-        original: 'https://i.postimg.cc/BZhcVY21/laundry.jpg',
-        thumbnail: 'https://i.postimg.cc/BZhcVY21/laundry.jpg',
-      },
-      {
-          original: "https://i.postimg.cc/ZYc9nGCW/background.jpg",
-          thumbnail: "https://i.postimg.cc/ZYc9nGCW/background.jpg",
-        },
-        {
-          original: 'https://i.postimg.cc/8c9VKd1f/parkingarea2.jpg',
-          thumbnail: 'https://i.postimg.cc/8c9VKd1f/parkingarea2.jpg',
-        }
-        ,
-        {
-          original: 'https://i.postimg.cc/26sqqhfx/courtyard-2.jpg',
-          thumbnail: 'https://i.postimg.cc/26sqqhfx/courtyard-2.jpg',
-        },
-        {
-          original: 'https://i.postimg.cc/8c9VKd1f/parkingarea2.jpg',
-          thumbnail: 'https://i.postimg.cc/8c9VKd1f/parkingarea2.jpg',
-        }
-        ,
-        {
-          original: 'https://i.postimg.cc/6QSqSnTN/Kitchen.jpg',
-          thumbnail: 'https://i.postimg.cc/6QSqSnTN/Kitchen.jpg',
-        }
-  ];
+  {
+    original: "https://i.postimg.cc/7LvNN50s/kitchen.jpg",
+    thumbnail: "https://i.postimg.cc/7LvNN50s/kitchen.jpg",
+  },
+  {
+    original: 'https://i.postimg.cc/pT5hcCvK/living-Room.jpg',
+    thumbnail: 'https://i.postimg.cc/pT5hcCvK/living-Room.jpg',
+  },
+  {
+    original: 'https://i.postimg.cc/PrVLH0q1/lounge2.jpg"',
+    thumbnail: 'https://i.postimg.cc/PrVLH0q1/lounge2.jpg"',
+  },
+  {
+    original: "https://i.postimg.cc/L8FqCLQ1/friends.jpg",
+    thumbnail: "https://i.postimg.cc/L8FqCLQ1/friends.jpg",
+  },
+  {
+    original: 'https://i.postimg.cc/vZnrDGkW/room.jpg',
+    thumbnail: 'https://i.postimg.cc/vZnrDGkW/room.jpg',
+  },
+  {
+    original: 'https://i.postimg.cc/yYD1mxVS/happy-Insidethe-Room.jpg',
+    thumbnail: 'https://i.postimg.cc/yYD1mxVS/happy-Insidethe-Room.jpg',
+  },
+  {
+    original: 'https://i.postimg.cc/BZhcVY21/laundry.jpg',
+    thumbnail: 'https://i.postimg.cc/BZhcVY21/laundry.jpg',
+  },
+  {
+    original: "https://i.postimg.cc/ZYc9nGCW/background.jpg",
+    thumbnail: "https://i.postimg.cc/ZYc9nGCW/background.jpg",
+  },
+  {
+    original: 'https://i.postimg.cc/8c9VKd1f/parkingarea2.jpg',
+    thumbnail: 'https://i.postimg.cc/8c9VKd1f/parkingarea2.jpg',
+  }
+  ,
+  {
+    original: 'https://i.postimg.cc/26sqqhfx/courtyard-2.jpg',
+    thumbnail: 'https://i.postimg.cc/26sqqhfx/courtyard-2.jpg',
+  },
+  {
+    original: 'https://i.postimg.cc/8c9VKd1f/parkingarea2.jpg',
+    thumbnail: 'https://i.postimg.cc/8c9VKd1f/parkingarea2.jpg',
+  }
+  ,
+  {
+    original: 'https://i.postimg.cc/6QSqSnTN/Kitchen.jpg',
+    thumbnail: 'https://i.postimg.cc/6QSqSnTN/Kitchen.jpg',
+  }
+];
 
 
 const Facilities = () => (
-    <div>    
-            <Container fluid class="main-body">   
-            <h1>Our Facilities</h1> 
-            <div id='facilitiesGallery'>
-            <ImageGallery  items={images} alt='facilities inside otautahi house'/>
-            </div>
+  <div>
+    <Container fluid class="main-body">
+      <h1>Our Facilities</h1>
+      <div id='facilitiesGallery'>
+        <ImageGallery items={images} alt='facilities inside otautahi house' />
+      </div>
 
-                <Row><Col lg={8}></Col></Row>     
-                <Row >
-                    <Col sm={12} lg={4}>
-                    
-                    
-                    <h2 class='facility-heading'>Common Spaces</h2>
-                    <ul><li>The communal kitchens have a fridge/freezer, microwave, toaster, electric jug and stove</li>
-                    <li>Bathroom amenities consist of two showers, two vanity units and two toilets</li>
-                    </ul>
-                    
-                    
-                    </Col>
-                    <Col sm={12} lg={4}>
-                    <h2 class='facility-heading'>Private Room</h2>
-                    
-                    {/* https://www.npmjs.com/package/react-slideshow-image */}
-                    <ul><li>You have your own room which is furnished with a king single bed, desk, chair, wardrobe, mirror and drawers.</li>
-                    </ul>
-                    
-                    </Col>
-                    <Col sm={12} lg={4}>
-                    {/* https://www.npmjs.com/package/react-slideshow-image */}
-                    <h2 class='facility-heading'>Other Amenities</h2>
-                    <ul>
-                    <li>Coin operated laundry facilities on the 2nd and 3rd floors of blocks A and B.</li>
-                    <li>Wi-Fi is included in all blocks.</li>
-                    <li>Although there are limited slots, parking is available within vicinity.</li>
-                    </ul>
-                    
-                    </Col>
-                </Row>
-            </Container> 
-    </div>
+      <Row><Col lg={8}></Col></Row>
+      <Row >
+        <Col sm={12} lg={4}>
+
+
+          <h2 class='facility-heading'>Common Spaces</h2>
+          <ul><li>The communal kitchens have a fridge/freezer, microwave, toaster, electric jug and stove</li>
+            <li>Bathroom amenities consist of two showers, two vanity units and two toilets</li>
+          </ul>
+
+
+        </Col>
+        <Col sm={12} lg={4}>
+          <h2 class='facility-heading'>Private Room</h2>
+
+          <ul><li>You have your own room which is furnished with a king single bed, desk, chair, wardrobe, mirror and drawers.</li>
+          </ul>
+
+        </Col>
+        <Col sm={12} lg={4}>
+          <h2 class='facility-heading'>Other Amenities</h2>
+          <ul>
+            <li>Coin operated laundry facilities on the 2nd and 3rd floors of blocks A and B.</li>
+            <li>Wi-Fi is included in all blocks.</li>
+            <li>Although there are limited slots, parking is available within vicinity.</li>
+          </ul>
+
+        </Col>
+      </Row>
+    </Container>
+  </div>
 
 )
 
 export default Facilities;
-
-// https://www.npmjs.com/package/reactjs-image-gallery  https://openbase.com/js/react-image-gallery
